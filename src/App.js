e1ab9549e324f9b52c8e442917ce1928455a4aae
github user name <matsuking0428@gmail.com>
2022-04-05 21:49:45 +0900

    more-jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
-function App() {
-  return React.createElement(
-    "h1",
-    null,
-    "Hello, world!"
-  );
+class App extends Component {
+  render() {
+    return (
+    <div>
+       <label htmlFor="bar">bar</label>
+       <input type="text" onChange={()=> {console.log("I am clicked.")}}/>
+    </div>
+    )
+  }
 }
 
 export default App;
