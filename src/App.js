3f41fb8a407f215edf3cd1064a6cf1a84ee250a8
github user name <matsuking0428@gmail.com>
2022-04-06 08:03:42 +0900

    components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-    <div>
-       <label htmlFor="bar">bar</label>
-       <input type="text" onChange={()=> {console.log("I am clicked.")}}/>
-    </div>
-    )
-  }
+const App = () => {
+   return (
+      <div>
+         <Cat />
+         <Cat />
+         <Cat />
+         <Cat />
+      </div>
+   )
+}
+
+const Cat = () => {
+   return <div>OOOOOOOO!</div>
 }
 
 export default App;
