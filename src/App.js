89d8519821ad94829958c7ae09e28bf85a9ee86e
github user name <matsuking0428@gmail.com>
2022-04-06 11:23:27 +0900

    setstate

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
-//<User name={"Taro"}/> Taroという属性を与える。
-const App = () => {
-   const profiles = [
-      {name: "Taro", age:10},
-      {name: "Hanako", age:30},
-      {name: "aaa", age: 56}
-   ]
-   return (
-       <div>
-         {
-           profiles.map((profile, index) => {
-             return <User name={profile.name} age={profile.age} key={index}/>
-           })
-         }
-       </div>
-   )
+const App = () => (<Counter> </Counter>);
+
+
+class Counter extends Component {
+   constructor(props){
+      super(props)
+      this.state = { count: 0}
+   }
+
+   handlePlusButtun = () => {
+      this.setState({ count: this.state.count + 1});
+   }
+
+   handleMinusButtun = () => {
+      this.setState({ count: this.state.count - 1});
+   }
+
+   render () {
+     return (
+        <React.Fragment>
+           <div> count: {this.state.count}</div>
+           <button onClick={this.handlePlusButtun}>+1</button>
+           <button onClick={this.handleMinusButtun}>-1</button>
+        </React.Fragment>
+     )
+   }
 }
 
-const User = (props) => {
-   return <div>Hi, I am {props.name}, and {props.age} years old</div> // conponentsは使いまわしできる
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
 export default App;
