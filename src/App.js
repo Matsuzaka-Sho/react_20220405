963a8b75174b474011c6378c24255ab105030122
github user name <matsuking0428@gmail.com>
2022-04-06 10:53:13 +0900

    props-type

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 //<User name={"Taro"}/> Taroという属性を与える。
 const App = () => {
    const profiles = [
       {name: "Taro", age:10},
       {name: "Hanako", age:30},
-      {name: "aaa"}
+      {name: "aaa", age: 56}
    ]
    return (
        <div>
@@ -22,7 +23,8 @@ const User = (props) => {
    return <div>Hi, I am {props.name}, and {props.age} years old</div> // conponentsは使いまわしできる
 }
 
-User.defaultProps = {
-  age: 1
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
 export default App;
